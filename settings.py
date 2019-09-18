f11c3e17f9a57b20cfc650e3bcd8f1a7b2d895b0
chicm <idv2005@gmail.com>
2019-09-18 17:06:37 +0000

    first commit

---
+++ settings.py
@@ -0,0 +1,8 @@
+import os.path as osp
+
+ROOT_DIR = '/data2/yt8m/3/frame'
+META_DIR = ROOT_DIR
+VAL_NPY_DIR = osp.join(ROOT_DIR, 'val_npy')
+TEST_NPY_DIR = osp.join(ROOT_DIR, 'test_npy')
+TRAIN_NPY_DIR = '/data2/yt8m/2/frame/train_single_npy'
+MODEL_DIR = './work_dirs'
